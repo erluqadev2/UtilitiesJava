cd6bc204c064c282c46bab06e872a6ff33203ef1
Ericson Luis Quispe Abad <erluqadev@gmail.com>
2017-06-26 21:31:39 -0500

    test replicatefiles

--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -22,32 +22,9 @@ public class Utilities {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-//        String path = "C:\\Users\\Editorial_03\\Desktop\\list_files.txt";
-//        try {
-//            List<String> list = HelperFiles.readFileInList(path);
-//            System.out.println("numero de archivos : " + list.size());
-//            List<String> listWithoutRepeating = HelperList.clearRepeat(list);
-//            System.out.println("ahora hay : " + listWithoutRepeating.size());
-//            String newPath = HelperFiles.newPathDirectory(path);
-//            HelperFiles.writeListInFile(listWithoutRepeating, newPath);
-//            List<String> listDeleting = HelperList.getRepeatingList(list);
-//            System.out.println("count repeating : " + listDeleting.size());
-//            for(String str : listDeleting){
-//                System.out.println(str);
-//            }
-//        } catch (IOException ex) {
-//            Logger.getLogger(Utilities.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (Exception ex) {
-//            Logger.getLogger(Utilities.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-
-
-        String path = "D:\\others";
         try {
-            long size = HelperFiles.sizeFile(path);
-            System.out.println(HelperFiles.sizeFileToString(size));
+            // TODO code application logic here
+            HelperFiles.replicateFiles("D:\\por_firmar", 100);
         } catch (Exception ex) {
             Logger.getLogger(Utilities.class.getName()).log(Level.SEVERE, null, ex);
         }
